babcfd78ffc8306749094c6633bfbfd0c22b1b45
Your Name <you@example.com>
2024-06-30 19:09:41 -0500

    Initial commit

---
+++ src/main/java/s3eloader/S3ELoaderLoader.java
@@ -0,0 +1,231 @@
+/* ###
+ * IP: GHIDRA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package s3eloader;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.*;
+
+import ghidra.app.util.Option;
+import ghidra.app.util.bin.ByteProvider;
+import ghidra.app.util.importer.MessageLog;
+import ghidra.app.util.opinion.AbstractProgramWrapperLoader;
+import ghidra.app.util.opinion.LoadSpec;
+import ghidra.framework.model.DomainObject;
+import ghidra.program.model.listing.Data;
+import ghidra.program.model.listing.Program;
+import ghidra.program.model.mem.Memory;
+import ghidra.program.model.mem.MemoryBlock;
+import ghidra.program.model.symbol.SymbolTable;
+import ghidra.program.model.reloc.RelocationTable;
+import ghidra.program.database.mem.FileBytes;
+import ghidra.program.model.address.Address;
+import ghidra.program.model.address.AddressFactory;
+import ghidra.program.model.lang.LanguageCompilerSpecPair;
+import ghidra.program.model.symbol.ExternalManager;
+import ghidra.program.model.symbol.SourceType;
+import ghidra.app.util.bin.BinaryReader;
+import ghidra.program.flatapi.FlatProgramAPI;
+import ghidra.util.exception.CancelledException;
+import ghidra.util.task.TaskMonitor;
+
+/**
+ * TODO: Provide class-level documentation that describes what this loader does.
+ */
+public class S3ELoaderLoader extends AbstractProgramWrapperLoader {
+
+	@Override
+	public String getName() {
+
+		// TODO: Name the loader.  This name must match the name of the loader in the .opinion 
+		// files.
+
+		return "S3ELoader";
+	}
+
+	@Override
+	public Collection<LoadSpec> findSupportedLoadSpecs(ByteProvider provider) throws IOException {
+		BinaryReader br = new BinaryReader(provider, true);
+		
+		List<LoadSpec> loadSpecs = new ArrayList<>();
+
+		// TODO: Examine the bytes in 'provider' to determine if this loader can load it.  If it 
+		// can load it, return the appropriate load specifications.
+		//if (br.readNextByteArray(4) == (("XE3U").getBytes())) {
+			loadSpecs.add(new LoadSpec(this, 0, new LanguageCompilerSpecPair("ARM:LE:32:v6", "default"), true));
+		//}
+		
+		// !! TODO !! We should find the header and read what arch to use
+
+		return loadSpecs;
+	}
+
+	@Override
+	protected void load(ByteProvider provider, LoadSpec loadSpec, List<Option> options,
+			Program program, TaskMonitor monitor, MessageLog log)
+			throws CancelledException, IOException {
+
+		// TODO: Load the bytes from 'provider' into the 'program'.
+		
+		FlatProgramAPI api = new FlatProgramAPI(program, monitor);
+		
+		Memory memory = program.getMemory();
+		InputStream ist = provider.getInputStream(0);
+		//SymbolTable symbols = program.getSymbolTable();
+		//RelocationTable relocations = program.getRelocationTable();
+		
+		// read that sick s3e header
+		BinaryReader br = new BinaryReader(provider, true);
+		int header = br.readNextInt();
+		int version = br.readNextInt();
+		short flags = br.readNextShort();
+		short arch = br.readNextShort();
+		int fixupOffset = br.readNextInt();
+		int fixupSize = br.readNextInt();
+		int codeOffset = br.readNextInt();
+		int codeFileSize = br.readNextInt();
+		int codeMemSize = br.readNextInt();
+		int sigOffset = br.readNextInt();
+		int sigSize = br.readNextInt();
+		int entryOffset = br.readNextInt();
+		int configOffset = br.readNextInt();
+		int configSize = br.readNextInt();
+		long baseAddrOrig = br.readNextInt(); // this is a long since we need the full address range lmao
+		int extraOffset = br.readNextInt();
+		int extraSize = br.readNextInt();
+		int extHeaderSize = br.readNextInt(); // TODO should be used
+		int realCodeSize = br.readNextInt(); // TODO should be dynamic
+		int showSplash = br.readNextInt(); // TODO should be dynamic
+		
+		int bssSize = codeMemSize - codeFileSize;
+		
+		/**
+		 * Load the program bytes into ghidra's representation of memory
+		 */
+		FileBytes fb = memory.createFileBytes(provider.getName(), 0, provider.length(), ist, monitor);
+		
+		try {
+			MemoryBlock block = memory.createInitializedBlock("code", api.toAddr(baseAddrOrig), fb, codeOffset, realCodeSize, false);
+			block.setRead(true);
+			block.setWrite(false);
+			block.setExecute(true);
+			
+			MemoryBlock data = memory.createInitializedBlock("data", api.toAddr(baseAddrOrig + realCodeSize), fb, codeOffset + realCodeSize, codeFileSize - realCodeSize, false);
+			data.setRead(true);
+			data.setWrite(true);
+			data.setExecute(false);
+			
+			MemoryBlock bss = memory.createInitializedBlock("bss", api.toAddr(baseAddrOrig + codeFileSize), bssSize, (byte) 0x0, monitor, false);
+			bss.setRead(true);
+			bss.setWrite(true);
+			bss.setExecute(false);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		/**
+		 * Read the load table.
+		 */
+		List<String> symbolNames = new ArrayList<>();
+		br.setPointerIndex(fixupOffset);
+		ExternalManager extman = program.getExternalManager();
+		
+		while (br.getPointerIndex() < (fixupOffset + fixupSize)) {
+			int fixupSectionOffset = (int) br.getPointerIndex();
+			int fixupSectionType = br.readNextInt();
+			int fixupSectionSize = br.readNextInt() - 8;
+			
+			System.out.printf("fixup section type %d size 0x%x\n", fixupSectionType, fixupSectionSize);
+			
+			switch (fixupSectionType) {
+				case 0: {
+					int symbolCount = br.readNextShort();
+					
+					System.out.printf("Loading %d symbol names...\n", symbolCount);
+					
+					for (int i = 0; i < symbolCount; i++) {
+						symbolNames.add(br.readNextAsciiString());
+					}
+					
+					break;
+				}
+				
+				case 2:
+				case 3:
+				case 4: {
+					int extRelocCount = br.readNextInt();
+					
+					try {
+						for (int i = 0; i < extRelocCount; i++) {
+							int hi = br.readNextUnsignedShort();
+							int lo = br.readNextUnsignedShort();
+							long offset = (hi << 16) | lo;
+							int symbolIndex = br.readNextUnsignedShort();
+							
+							
+							System.out.printf("create extref 0x%x (offset 0x%x) for %s\n", baseAddrOrig + offset, offset, symbolNames.get(symbolIndex));
+							
+							Data data = api.createDWord(api.toAddr(baseAddrOrig + offset));
+							api.createExternalReference(data, "libs3e_android.so", symbolNames.get(symbolIndex), null);
+						}
+					}
+					catch (Exception e) {
+						e.printStackTrace();
+					}
+					
+					break;
+				}
+				
+				default: {
+					break;
+				}
+			}
+			
+			br.setPointerIndex(fixupSectionOffset + fixupSectionSize + 8);
+		}
+		
+		/**
+		 * Finalise stuff
+		 */
+		api.addEntryPoint(api.toAddr(baseAddrOrig + entryOffset));
+		api.disassemble(api.toAddr(baseAddrOrig + entryOffset));
+		api.createFunction(api.toAddr(baseAddrOrig + entryOffset), "__s3e_app_main");
+		
+		ist.close();
+	}
+
+	@Override
+	public List<Option> getDefaultOptions(ByteProvider provider, LoadSpec loadSpec,
+			DomainObject domainObject, boolean isLoadIntoProgram) {
+		List<Option> list =
+			super.getDefaultOptions(provider, loadSpec, domainObject, isLoadIntoProgram);
+
+		// TODO: If this loader has custom options, add them to 'list'
+		//list.add(new Option("Option name goes here", "Default option value goes here"));
+
+		return list;
+	}
+
+	@Override
+	public String validateOptions(ByteProvider provider, LoadSpec loadSpec, List<Option> options, Program program) {
+
+		// TODO: If this loader has custom options, validate them here.  Not all options require
+		// validation.
+
+		return super.validateOptions(provider, loadSpec, options, program);
+	}
+}
